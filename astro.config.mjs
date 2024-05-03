4e97d249610a77346694d33cd97dde9f8744d870
Christoph <christoph.saile@googlemail.com>
2024-05-04 00:22:49 +0200

    feat: init project

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import lit from "@astrojs/lit";
+import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [lit(), tailwind()],
+  output: "server",
+  adapter: netlify()
+});
