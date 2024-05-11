d4a2e6f97cba1d1257dbd120d75c2d983bb45d13
Christoph <christoph.saile@googlemail.com>
2024-05-11 11:47:17 +0200

    feat: add filter option

--- src/components/spotify/TimeFilters.svelte
+++ src/components/spotify/TimeFilters.svelte
@@ -1,16 +1,30 @@
-<script>
-	export let title;
+<script lang="ts">
+  import { type TimeRange } from '../../libs/spotify';
+
+  export let title;
+  let elem: HTMLElement;
+
+  function handleClick(period: TimeRange) {
+    elem
+      .closest('.ranked-section')
+      ?.querySelectorAll('.ranked-list')
+      .forEach((el) => {
+        el.classList.contains(period) ? el.classList.remove('hidden') : el.classList.add('hidden');
+      });
+  }
 </script>
 
-<h2>{title}</h2>
-<ul>
-  <li>
-    <button>recent</button>
-  </li>
-  <li>
-    <button>last 6 months</button>
-  </li>
-  <li>
-    <button>all time</button>
-  </li>
-</ul>
+<div bind:this={elem}>
+  <h2>{title}</h2>
+  <ul class="flex">
+    <li>
+      <button on:click={() => handleClick('short_term')}>recent</button>
+    </li>
+    <li>
+      <button on:click={() => handleClick('medium_term')}>last 6 months</button>
+    </li>
+    <li>
+      <button on:click={() => handleClick('long_term')}>all time</button>
+    </li>
+  </ul>
+</div>
