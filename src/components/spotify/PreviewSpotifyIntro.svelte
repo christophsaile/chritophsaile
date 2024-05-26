45bb38122ba0357f882a21c195548b133f021684
Christoph <christoph.saile@googlemail.com>
2024-05-26 14:05:40 +0200

    feat: finetune spacings

--- src/components/spotify/PreviewSpotifyIntro.svelte
+++ src/components/spotify/PreviewSpotifyIntro.svelte
@@ -7,12 +7,28 @@
 {#await tracksPromise then tracks}
   <div class="grid grid-cols-2 gap-5 sm:flex sm:flex-wrap sm:justify-between sm:gap-8">
     {#each tracks.items as track (track.id)}
-      <Tile
-        title={track.name}
-        description={track.artists.map((artist) => artist.name).join(', ')}
-        img={track.album.images}
-        externalUrl={track.external_urls.spotify}
-      />
+      <div class="tile">
+        <Tile
+          title={track.name}
+          description={track.artists.map((artist) => artist.name).join(', ')}
+          img={track.album.images}
+          externalUrl={track.external_urls.spotify}
+        />
+      </div>
     {/each}
   </div>
 {/await}
+
+<style>
+  .tile:last-child,
+  .tile:nth-last-child(2) {
+    display: none;
+  }
+
+  @media (min-width: 896px) {
+    .tile:last-child,
+    .tile:nth-last-child(2) {
+      display: block;
+    }
+  }
+</style>
--- src/components/spotify/Tile.svelte
+++ src/components/spotify/Tile.svelte
@@ -7,7 +7,7 @@
   export let externalUrl = '';
 </script>
 
-<a class="grid-item max-w-40" href={externalUrl}>
+<a class="max-w-40" href={externalUrl}>
   <img src={img[1]?.url || img[0].url} alt={title} height={160} width={160} />
   <div>
     <p class="font-bold">{title}</p>
@@ -16,17 +16,3 @@
     {/if}
   </div>
 </a>
-
-<style>
-  .grid-item:last-child,
-  .grid-item:nth-last-child(2) {
-    display: none;
-  }
-
-  @media (min-width: 896px) {
-    .grid-item:last-child,
-    .grid-item:nth-last-child(2) {
-      display: block;
-    }
-  }
-</style>
