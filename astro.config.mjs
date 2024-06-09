a726c7f49a97e7eac6f8e8449af65c6690031597
Christoph <christoph.saile@googlemail.com>
2024-06-09 12:44:13 +0200

    feat: add mdx integrartion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
-
 import svelte from '@astrojs/svelte';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    svelte(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), svelte(), mdx()],
   output: 'hybrid',
   adapter: netlify(),
   image: {
-    domains: ['open.spotify.com', 'i.scdn.co'],
-  },
+    domains: ['open.spotify.com', 'i.scdn.co']
+  }
 });
