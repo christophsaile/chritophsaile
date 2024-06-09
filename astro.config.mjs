9e59b13edd80f2901d4dfb15ba343a757451ca82
Christoph <christoph.saile@googlemail.com>
2024-06-09 13:48:13 +0200

    refactor: move content into mdx files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,20 @@ import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify';
 import svelte from '@astrojs/svelte';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), svelte(), mdx()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    svelte(),
+    mdx(),
+  ],
   output: 'hybrid',
   adapter: netlify(),
   image: {
-    domains: ['open.spotify.com', 'i.scdn.co']
-  }
+    domains: ['open.spotify.com', 'i.scdn.co'],
+  },
 });
