72e296aa3f3a2e20dd9e7f62312dfdce0f133e3d
Christoph <christoph.saile@googlemail.com>
2024-06-22 23:26:09 +0200

    feat: add vinyl lp to spotify

--- src/components/latest-music/tile.svelte
+++ src/components/latest-music/tile.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { SpotifyImage } from '../../libs/spotify';
+  import vinyl from '../../assets/vinyl.webp';
 
   export let title = '';
   export let description = '';
@@ -7,14 +8,23 @@
   export let externalUrl = '';
 </script>
 
-<a class="max-w-40" href={externalUrl}>
-  <img
-    class="rounded-sm"
-    src={img[1]?.url || img[0].url}
-    alt={title}
-    height={160}
-    width={160}
-  />
+<a class="group max-w-40 relative" href={externalUrl}>
+  <div class="group relative w-fit">
+    <img
+      class="rounded-sm relative z-10"
+      src={img[1]?.url || img[0].url}
+      alt={title}
+      height={160}
+      width={160}
+    />
+    <img
+      class="absolute left-0 top-0 opacity-0 group-hover:opacity-100 transition-all group-hover:left-[24%]"
+      src={vinyl.src}
+      alt=""
+      height={160}
+      width={160}
+    />
+  </div>
   <div>
     <p class="font-bold">{title}</p>
     {#if description}
