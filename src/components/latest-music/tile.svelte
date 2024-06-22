22df49a9386bdabbf302b9ea58621e8cd261b066
Christoph <christoph.saile@googlemail.com>
2024-06-22 23:27:55 +0200

    fix: z-index vinyl

--- src/components/latest-music/tile.svelte
+++ src/components/latest-music/tile.svelte
@@ -11,14 +11,14 @@
 <a class="group max-w-40 relative" href={externalUrl}>
   <div class="group relative w-fit">
     <img
-      class="rounded-sm relative z-10"
+      class="rounded-sm"
       src={img[1]?.url || img[0].url}
       alt={title}
       height={160}
       width={160}
     />
     <img
-      class="absolute left-0 top-0 opacity-0 group-hover:opacity-100 transition-all group-hover:left-[24%]"
+      class="absolute left-0 top-0 opacity-0 group-hover:opacity-100 transition-all group-hover:left-[24%] -z-10"
       src={vinyl.src}
       alt=""
       height={160}
